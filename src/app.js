f8d5a393179b458eeee340fe41249ab6ab76bbf2
Laura Spieker <ljspiek@gmail.com>
2020-02-04 19:38:30 -0600

    created bookmarks router and added middleware

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
+const logger = require('./logger')
+const winston = require('winston')
+const uuid = require('uuid/v4')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
 
 const app = express()
 
@@ -11,13 +15,25 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
+
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors())
 
-app.get('/', (req, res) => {
-    res.send('Hello, world!')
+app.use(function validateBearerToken(req, res, next) {
+    const apiToken = process.env.API_TOKEN
+    const authToken = req.get('Authorization')
+    
+    if(!authToken || authToken.split(' ')[1] !== apiToken) {
+        logger.error(`Unauthorized request to path: ${req.path}`);
+        console.log(apiToken, authToken);
+        return res.status(401).json({ error: 'Unauthorized request'})
+    }
+    next()
 })
+
+
+app.use(bookmarksRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
